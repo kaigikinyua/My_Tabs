b332d8cdf7882d34184cab2652dc148fda4a06cb
kaigikinyua <kaigikinyua@gmail.com>
2020-03-10 14:17:30 +0300

    synching curr_video and audio to current media

--- server/static/JS/mutimedia.js
+++ server/static/JS/mutimedia.js
@@ -1,7 +1,7 @@
 baseUrl="http://localhost:4000"
 
 currentPlaylist=""
-currentMedia={time:0}
+currentMedia={media:"",volume:0.5,time:0,title:""}
 
 
 
@@ -35,8 +35,17 @@ function getAudio(){
 
 function playSong(){
     var song=document.getElementById('curr_song')
+    if(currentMedia.media!=song){
+        try {
+            currentMedia.media.pause()
+        } catch (error) {
+            
+        }
+        currentMedia.media=song
+        currentMedia.time=0
+    }
     song.currentTime=currentMedia.time
-    song.play()
+    currentMedia.media.play()
     var play=document.getElementById('play_audio')
     play.classList.add('hidden')
     var pause=document.getElementById('pause_audio')
@@ -53,21 +62,82 @@ function pauseSong(){
 }
 function volumeUp(){
     var song=document.getElementById('curr_song')
-    song.volume+=0.1
+    if(song.volume<1){
+        song.volume+=0.1
+    }
 }
 function volumeDown(){
     var song=document.getElementById('curr_song')
-    song.volume-=0.1
+    if(song.volume>0.1){
+        song.volume-=0.1
+    }
 }
 
+
+//current media functions
+
+
+
+function playVideo(){
+    var video=document.getElementById('curr_video')
+    if(currentMedia.media!=video){
+        try {
+            pauseSong()
+            currentMedia.media.pause()
+            currentMedia.time=0
+        } catch (error) {
+            
+        }
+        currentMedia.media=video
+        currentMedia.time=0
+    }
+    currentMedia.media=document.getElementById('video_min')
+    currentMedia.media.src=video.src
+    currentMedia.media.volume=0
+    currentMedia.media.play()
+    video.play()
+
+    //update controls
+    var video_play=document.getElementById('video_play')
+    var video_pause=document.getElementById('video_pause')
+    video_play.classList.add('hidden')
+    video_pause.classList.remove('hidden')
+}
+
+function pauseVideo(){
+    var video=document.getElementById('curr_video')
+    currentMedia.time=video.currentTime
+    video.pause()
+    currentMedia.media.pause()
+    pauseCurr()
+    var video_play=document.getElementById('video_play')
+    var video_pause=document.getElementById('video_pause')
+    video_play.classList.remove('hidden')
+    video_pause.classList.add('hidden')
+
+}
+//curr media functions
+function playCurr(){
+    try{
+        currentMedia.media.play()
+    }catch{
+
+    }
+}
+function pauseCurr(){
+    try{
+    currentMedia.media.pause()
+    }catch{
+
+    }
+}
+function nextMedia(){}
 
 function updateCurrentMediaTime(){
 
 }
 
 /* module 2
-function play(){}
-function pause(){}
 function next(){}
 */
 
