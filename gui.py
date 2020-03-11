2774ec0c65afb45988b8347530d92cff4ff1b9d6
kaigikinyua <kaigikinyua@gmail.com>
2020-03-11 19:59:05 +0300

    working on desktop module

---
+++ gui.py
@@ -0,0 +1,11 @@
+
+
+class Output:
+    
+    @staticmethod
+    def display_error(err_message):
+        print("Error:-> "+str(err_message))
+    
+    @staticmethod
+    def success(message):
+        print("Success:-> "+str(message))
--- myapp.py
+++ myapp.py
@@ -1,7 +1,10 @@
-
 import os
 import threading
 import webbrowser
+import time
+
+
+from gui import Output
 
 class MyApp:
     def __init__(self):
@@ -24,3 +27,34 @@ class MyApp:
             print("Could not start server")
         else:
             self.server_running=True
+
+    def add_file(self):
+        file_path=input("Enter filename\n")
+        if(os.path.isfile(file_path)):
+            Output.success("adding file")
+        else:
+            Output.display_error("Selected file path is not a file")
+
+    def add_directory(self):
+        folder_path=input("Enter folder path\n")
+        if(os.path.isdir(folder_path)):
+            Output.success("Adding files in dir "+str(folder_path))
+            files=os.listdir(folder_path)
+            for file in files:
+                print("File "+str(file))
+                time.sleep(0.5)
+        else:
+            Output.display_error("Selected folder does not exist")
+
+    
+    def flow(self):
+        ans=input("What do you want to do\n 1.Add file\n2.Add a folder\n")
+        if(ans=="1"):
+            self.add_file()
+        elif(ans=="2"):
+            self.add_directory()
+        else:
+            print("Unknown choice")
+
+M=MyApp()
+M.flow()
